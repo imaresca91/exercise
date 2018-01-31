0fa6af704e07a2c6d4deb9acbb9cc210b91b70c0
Nacho Maresca <nachomaresca@MacBook-Pro-de-Nacho.local>
2018-01-31 11:54:01 -0300

    test: wrap up project
    - test all pages and javascripts.
    - add 1 more indicator in the country carousel so there is one per image.
    - remove the social networks in the side drawer because there are at the footer.

--- js/routes.js
+++ js/routes.js
@@ -1,10 +1,10 @@
 function searchMobileHome(){
-    window.location.href = window.location.origin + '/~nachomaresca/traveler/home-mobile.html';
+    window.location.href = window.location.origin + '/home-mobile.html';
 }
 
 function searchCountry(id, name, flag, description){
     window.location.href = 
-    window.location.origin + '/~nachomaresca/traveler/country.html' +
+    window.location.origin + '/country.html' +
     '?id=' + id +
     '&name=' + name +
     '&flag=' + flag +
@@ -12,9 +12,9 @@ function searchCountry(id, name, flag, description){
 }
 
 function searchContactUs(){
-    window.location.href = window.location.origin + '/~nachomaresca/traveler/contactus.html';
+    window.location.href = window.location.origin + '/contactus.html';
 }
 
 function searchHome(){
-    window.location.href = window.location.origin + '/~nachomaresca/traveler/index.html';
+    window.location.href = window.location.origin + '/index.html';
 }
